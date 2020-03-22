8f6f22ae686f9db30b369d819256bf8fe6cd194f
温家荣 <wenjiarongs@163.com>
2020-03-22 10:52:29 +0800

    前后端联调-2020.03.22

---
+++ wjr-server/wjr-server-system/src/main/java/com/system/mapper/MenuMapper.java
@@ -0,0 +1,28 @@
+package com.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.common.entity.system.Menu;
+
+import java.util.List;
+
+/**
+ *  实现根据用户获取菜单信息，并将菜单转换为包含上下级结构的路由信息的功能
+ */
+public interface MenuMapper extends BaseMapper<Menu> {
+
+    /**
+     * 通过用户名查询权限信息
+     *
+     * @param username 用户名称
+     * @return 权限信息
+     */
+    List<Menu> findUserPermissions(String username);
+
+    /**
+     * 通过用户名查询菜单信息
+     *
+     * @param username 用户名
+     * @return 菜单信息
+     */
+    List<Menu> findUserMenus(String username);
+}
---
+++ wjr-server/wjr-server-system/src/main/java/com/system/service/IMenuService.java
@@ -0,0 +1,27 @@
+package com.system.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.common.entity.router.VueRouter;
+import com.common.entity.system.Menu;
+
+import java.util.List;
+import java.util.Set;
+
+public interface IMenuService extends IService<Menu> {
+
+    /**
+     * 通过用户名查询用户权限信息
+     *
+     * @param username 用户名
+     * @return 权限信息
+     */
+    Set<String> findUserPermissions(String username);
+
+    /**
+     * 通过用户名创建对应的 Vue路由信息
+     *
+     * @param username 用户名
+     * @return 路由信息
+     */
+    List<VueRouter<Menu>> getUserRouters(String username);
+}
