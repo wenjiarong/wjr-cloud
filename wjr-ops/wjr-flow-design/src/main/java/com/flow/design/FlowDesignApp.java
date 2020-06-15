88597436024247a78f0b94a7d7122968dcad6f6c
温家荣 <wenjiarongs@163.com>
2020-06-15 23:45:45 +0800

    整合flowable模块 -2020.06.15

---
+++ wjr-ops/wjr-flow-design/src/main/java/com/flow/design/FlowDesignApp.java
@@ -0,0 +1,21 @@
+package com.flow.design;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: FlowDesignApp
+ * @Description: alibabacloud
+ * @Author: 温家荣-wjr
+ * @Date: 2020/6/15 22:49
+ * @Version: 1.0
+ */
+
+@SpringBootApplication
+public class FlowDesignApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowDesignApp.class, args);
+    }
+
+}
